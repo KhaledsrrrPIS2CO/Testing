abe322108dddbf1a0c8775e2785b8c7cc3a3fb9f
Khaled <Khaled.ameen11@gmail.com>
2022-09-15 22:34:04 +0200

    ow to think deeper to make the code faster + JSON + UTF8

---
+++ JSON.py
@@ -0,0 +1,10 @@
+#JAVA script object notatio
+#client & server
+# python JSON linbrary
+import json
+print(dir(json))
+#load S F
+#Dump S dump F
+json_file_dir = "dir"
+movies = json.load(json_file_dir)
+json_file_dir.close()
